d45fa12b86d50d2d4386cbb85ed907b5821f3539
BruhMano <vanusha747@gmail.com>
2022-03-13 22:59:54 +0400

    final one

--- prjct/Scripts/activate.bat
+++ prjct/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=H:\Dev\SchPrj\another_one\prjct
+set VIRTUAL_ENV=H:\dev\schprj\another_one\prjct
 
 if not defined PROMPT set PROMPT=$P$G
 
